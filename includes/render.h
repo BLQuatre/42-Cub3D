2e3c93043174bf0cabbb872541fa07d4ce6f1b02
Jonathan Bergos <jbergos@student.42lehavre.fr>
2025-02-06 19:26:27 +0100

    + | Add render.h dir render & render.c

---
+++ includes/render.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   render.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbergos <jbergos@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/06 17:49:59 by jbergos           #+#    #+#             */
+/*   Updated: 2025/02/06 18:13:13 by jbergos          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RENDER_H
+# define RENDER_H
+
+# ifndef WIN_WIDTH
+#  define WIN_WIDTH 500
+# endif
+
+# ifndef WIN_HEIGHT
+#  define WIN_HEIGHT 500
+# endif
+
+# include "cub3d.h"
+#endif
