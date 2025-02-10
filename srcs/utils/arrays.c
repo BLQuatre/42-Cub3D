c653ee184ab63593671851bf595007400e09f061
Jonathan Bergos <jbergos@student.42lehavre.fr>
2025-02-10 16:43:22 +0100

    ~ | modify free array

--- srcs/utils/arrays.c
+++ srcs/utils/arrays.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   arrays.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cauvray <cauvray@student.42lehavre.fr>     +#+  +:+       +#+        */
+/*   By: jbergos <jbergos@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 23:14:53 by cauvray           #+#    #+#             */
-/*   Updated: 2025/02/10 13:32:08 by cauvray          ###   ########.fr       */
+/*   Updated: 2025/02/10 16:42:26 by jbergos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ void	array_free(char **array)
 	if (!array)
 		return ;
 	i = 0;
-	while (array[i++])
-		free(array[i]);
+	while (array[i])
+		free(array[i++]);
 	free(array);
 }
 
